37e4d6616f6a9cf575fd373922a0149a0c5cd369
Kirill Bertosh <kirillbertosh@gmail.com>
2018-04-25 16:51:03 +0300

    initial commit

---
+++ task1/src/main/java/taks1/Application.java
@@ -0,0 +1,8 @@
+package taks1;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ task1/src/main/java/taks1/Test/Test.java
@@ -0,0 +1,37 @@
+package taks1.Test;
+
+public class Test {
+
+    public void testByte() {
+
+    }
+
+    public void testShort() {
+
+    }
+
+    public void testChar() {
+
+    }
+
+    public void testInteger() {
+
+    }
+
+    public void testLong() {
+
+    }
+
+    public void testFloat() {
+
+    }
+
+    public void testDouble() {
+
+    }
+
+    public void testBoolean() {
+
+    }
+
+}
